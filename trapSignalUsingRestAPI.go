c5aa0aff6e1959a42a250eb05c69f2c9daa3c9a4
baymax19 <baymax19@protonmail.com>
2019-04-27 13:39:16 +0530

    TrapSignalUsingRestAPI

---
+++ trapSignalUsingRestAPI.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+    "net/http"
+    "fmt"
+    "log"
+    "context"
+    "os"
+    "sync"
+    
+)
+
+type contextGlobal struct {
+    sync.Mutex
+    internal *context.CancelFunc
+}
+
+func newContextGlobal() *contextGlobal{
+    _, cancel := context.WithCancel(context.Background())
+    return &contextGlobal{
+        internal : &cancel,
+    }
+}
+
+func (c *contextGlobal) setContext(cancel context.CancelFunc){
+        c.Lock()
+        c.internal = &cancel
+        c.Unlock()
+}   
+
+func(c *contextGlobal) getContext() (value context.CancelFunc){
+    c.Lock();
+    value = *c.internal
+    c.Unlock();
+    return value
+}
+
+var userContext = newContextGlobal()
+
+func main(){
+
+    context, cancel := context.WithCancel(context.Background())
+    userContext.setContext(cancel)
+    
+    i:=0;
+    go func() {
+		for {
+			select {
+			case <-context.Done():
+				{
+					fmt.Printf("captured %v, exiting...\n",context.Done())
+					fmt.Println("iterations: ", i)
+					os.Exit(1)
+					return
+				}
+			default:
+				i = i + 1
+			}
+		}
+		
+	}()
+	
+	http.HandleFunc("/stop",trapSignal);
+    log.Fatal(http.ListenAndServe(":8080",nil))
+}
+
+func trapSignal(w http.ResponseWriter, r *http.Request){
+    userContext.getContext()()   
+}
